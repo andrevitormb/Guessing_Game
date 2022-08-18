d9b0e59b0528fb89a011815cb1a2cdbf1f0bb411
andrevitormb <100388131+andrevitormb@users.noreply.github.com>
2022-08-18 12:30:23 +0100

    refactoring code

--- main.js
+++ main.js
@@ -1,37 +1,40 @@
+//Variables
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
 
 const randomNumber = Math.round(Math.random() * 10 )
 let xAttempts = 1
 
-//Callback
+//events 
+
+btnTry.addEventListener('click', handleClick)
+btnReset.addEventListener('click', handleReset)
+
+//function
 function handleClick(event){
+    //Callback
+    
     event.preventDefault() //dont do the standard  
 
     const inputNumber = document.querySelector("#inputNumber")
 
     if(Number(inputNumber.value) == randomNumber) {
-        screen1.classList.add("hide")
-        screen2.classList.remove("hide")
+        
+        toggleScreen()
 
         screen2.querySelector("h2").innerText = `acertou em ${xAttempts} tentativas`
     }
 
     inputNumber.value = ""
     xAttempts++
-
 }
-
-//events 
-
-const btnTry = document.querySelector("#btnTry")
-const btnReset = document.querySelector("#btnReset")
-
-btnTry.addEventListener('click', handleClick)
-
-btnReset.addEventListener('click', function(){
-    screen1.classList.remove("hide")
-    screen2.classList.add("hide")
+function toggleScreen() {
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+}
+function handleReset(){
+    toggleScreen()
     xAttempts = 1
-})
-
+}
